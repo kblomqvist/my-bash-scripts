3d8834de05efc90e79efbf4951f4e57ea654e7e4
kblomqvist <kblomqvist@iki.fi>
2011-08-10 12:52:05 +0300

    Add mysql dump in dups3.sh

--- dups3.sh
+++ dups3.sh
@@ -21,38 +21,72 @@
 # Settings
 # ---------------------------------------------------------------------
 
-# Note that underscores '_' are not allowed in EU bucket names.
-DEST=s3+http://your.s3.bucket
+# Amazon AWS bucket name and folder if any, for example 'mybucket'
+# or 'mybucket/backups'
+BUCKET=
 
-# Export some ENV variables so you don't have to type anything
-export AWS_ACCESS_KEY_ID=YOUR_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=YOUR_SECRET_ACCESS_KEY
-export PASSPHRASE=YOUR_PASSPHRASE
+# Amazon AWS key id and secret key
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
 
-GPG_KEY=YOUR_GPG_KEY
+# Your GPG key which to use
+GPG_KEY=
+
+# Your GPG passphrase for the key defined above
+export PASSPHRASE=
+
+# MySQL access parameters. Be sure that you have MySQL privilege
+# to LOCK TABLES, otherwise database backup won't work.
+MYSQL_DB_USER=
+MYSQL_DB_PASSWORD=
+MYSQL_DB_HOST=localhost
+
+# Destination where to store MySQL dump file. The file will be named
+# as 'mysqldump.sql'.
+MYSQL_DUMP_DEST_DIR=/var/dbdumps
 
 # ---------------------------------------------------------------------
 # Program starts here
 # ---------------------------------------------------------------------
+
 if [ "$#" -eq 1 ] && [ $1 = "backup" ]; then
+
+	# Prepare MySQL dump
+	MYSQL_ACCESS_PARAMS=-h $MYSQL_DB_PASSWD -u $MYSQL_DB_USER \
+		-p$MYSQL_DB_PASSWORD
+
+	MYSQL_DUMP_FILE="$MYSQL_DUMP_DEST_DIR/mysqldump.sql"
+
+	if [ -z "${MYSQL_DATABASES+xxx}" ]; then
+		MYSQL_DATABASES=$(mysql $MYSQL_ACCESS_PARAMS -Bse "show databases")
+	fi
+
+	mkdir -p $MYSQL_DUMP_DEST_DIR
+	mysqldump $MYSQL_ACCESS_PARAMS $MYSQL_DATABASES > $MYSQL_DUMP_FILE \
+		&& chmod 600 $MYSQL_DUMP_FILE
+
+	# Run backups
 	duplicity \
 		--s3-use-new-style \
 		--encrypt-key=${GPG_KEY} \
 		--sign-key=${GPG_KEY} \
 		--include=/home \
 		--include=/etc \
+		--include=${MYSQL_DUMP_DEST_DIR} \
 		--exclude=/** \
 		--full-if-older-than 30D \
-		/ ${DEST}
+		/ s3+http://${BUCKET}
 
-	duplicity remove-older-than 6M --force ${DEST}
+	# Remove backups which are older than 6 month
+	duplicity remove-older-than 6M --force s3+http://${BUCKET}
+
 elif [ "$#" -eq 3 ] && [ $1 = "restore" ]; then
-	duplicity --file-to-restore $2 ${DEST} $3
+	duplicity --file-to-restore $2 s3+http://${BUCKET} $3
 else
 	echo "Usage:"
-	echo "	dups3 backup							  # Run backups"
+	echo "	dups3 backup                              # Run backups"
 	echo "	dups3 restore /home/my/foo.txt ~/restore  # Restore a file"
-	echo "	dups3 restore /home/my ~/restore		  # Restore a directory"
+	echo "	dups3 restore /home/my ~/restore          # Restore a directory"
 fi
 
 # Reset the ENV variables. Don't need them sitting around
